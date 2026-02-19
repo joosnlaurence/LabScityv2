35e3fbda1333a74be32da55903521a01cc91b45a
Liam Harvell <liam@harvell.us>
2026-02-19 11:10:14 -0500

    oopsies-again

--- src/components/chat/test_get_old.ts
+++ src/components/chat/test_get_old.ts
@@ -16,9 +16,6 @@ async function test() {
   console.log(result1);
 
   // WARN: The view in the database (chat_sidebar) uses the authid, so it cannot be tested here
-  const result2 = getChatsWithPreview("66d5fb76-fa15-4edf-8f1c-3b645465a94c", supabaseAdmin)
-
-  console.log(result2);
 }
 if (require.main === module) {
   test();
