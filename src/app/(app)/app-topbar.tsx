03e02350eee1045171f90c8c000f2bdf7ea94f5c
Rafael Niebles <rnieblesealo@gmail.com>
2026-02-24 11:11:18 -0500

    add logout functionality

--- src/app/(app)/app-topbar.tsx
+++ src/app/(app)/app-topbar.tsx
@@ -2,8 +2,20 @@
 
 import { Button, Menu } from "@mantine/core"
 import { IconLogout, IconSettings } from "@tabler/icons-react"
+import { useRouter } from "next/navigation"
+import { createClient } from "@/supabase/client"
 
 const LSAppTopBar = () => {
+  const router = useRouter()
+
+  const handleSignOut = async () => {
+    const supabase = createClient() // TODO: why dont we pass down the client?
+
+    await supabase.auth.signOut() // call signout
+
+    router.push("/login") // go to login screen
+  }
+
   return (
     <Menu>
       <Menu.Target>
@@ -22,9 +34,11 @@ const LSAppTopBar = () => {
 
         <Menu.Item
           c="red"
-          leftSection={<IconLogout size={14} />}>
-          Log Out
+          leftSection={<IconLogout size={14} />}
+          onClick={handleSignOut}>
+          Sign Out
         </Menu.Item>
+
       </Menu.Dropdown>
     </Menu>
   )
