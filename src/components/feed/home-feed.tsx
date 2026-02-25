904f3949c62f164e1e7787c3b74bfc64c4006d79
Christopher Ferreira <fartops34@gmail.com>
2026-02-25 18:21:41 -0500

    reAdd userName prop to PostCard component

--- src/components/feed/home-feed.tsx
+++ src/components/feed/home-feed.tsx
@@ -45,6 +45,7 @@ export function HomeFeed(props: HomeFeedProps) {
                   <PostCard
                     key={post.id}
                     userId={post.userId}
+                    userName={post.userName}
                     field={post.scientificField}
                     timeAgo={post.timeAgo}
                     content={post.content}
@@ -107,6 +108,7 @@ export function HomeFeed(props: HomeFeedProps) {
           <Stack key={post.id} className={classes.postStack} w="100%">
             <PostCard
               userId={post.userId}
+              userName={post.userName}
               field={post.scientificField}
               timeAgo={post.timeAgo}
               content={post.content}
