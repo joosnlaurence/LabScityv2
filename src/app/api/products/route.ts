7e3a3cbdaf24eff12e81d2b71f30ccb1a18b91d0
Anju Thomas <anjunoblethomas@gmail.com>
2026-05-16 11:17:52 -0400

    add GET route handlers for user publications and products

--- src/app/api/products/route.ts
+++ src/app/api/products/route.ts
@@ -1,55 +1,40 @@
 import { NextResponse } from "next/server";
 import { createClient } from "@/supabase/server";
-import type { ApiResponse, Product } from "@/lib/types/api";
+import type { ApiResponse } from "@/lib/types/api";
+import type { Product } from "@/lib/types/data";
 
-// this api is used to get your own products to put them on your profile
-export async function GET() {
+// GET /api/products?userId={userId}
+// returns all products that belong to the specified user 
+// query params = userId
+export async function GET(request: Request) {
+    const { searchParams } = new URL(request.url);
+    const userId = searchParams.get("userId");
+
+    if (!userId) return NextResponse.json<ApiResponse<Product[]>>(
+        { 
+            success: false, error: "userId required" 
+        }, { status: 400 }
+    );
+
     const supabase = await createClient();
-    const { data: authData } = await supabase.auth.getUser();
 
-    if (!authData.user) {
-        return NextResponse.json<ApiResponse<Product[]>>(
-            { success: false, error: "Authentication required" },
-            { status: 401 }
-        );
-    }
-
-    const { data: userProductData, error: userProductLinkError } = await supabase
-        .from("user_products")
-        .select("product_id")
-        .eq("user_id", authData.user.id);
-
-    if (userProductLinkError) {
-        return NextResponse.json(
-            { 
-                success: false, 
-                error: userProductLinkError.message 
-            }, { status: 500 }
-        );
-    }
-
-    // extracting product_id from each row in useProductData to get a plain array of ids
-    const userProductIds = (userProductData ?? []).map((row) => row.product_id);
-
-    // .in filters results based on if a column's value matches any item in the userProductIds array
     const { data, error } = await supabase
-        .from("products")
-        .select("*")
-        .in("product_id", userProductIds);
+        .from("user_products")
+        .select("products(*)")
+        .eq("user_id", userId)
+        .returns<{ products: Product }[]>();
 
-    if (error) {
-        return NextResponse.json(
-            { 
-                success: false, 
-                error: error.message 
-            }, { status: 500 }
-        );
-    }
+    if (error) return NextResponse.json<ApiResponse<Product[]>>(
+        { 
+            success: false, 
+            error: error.message 
+        }, { status: 500 }
+    );
 
-    return NextResponse.json<{success: boolean; data: Product[]}>(
+    return NextResponse.json<ApiResponse<Product[]>>(
         { 
             success: true, 
-            data: data ?? [] 
+            data: data.map((row) => row.products) 
         }
     );
 }
